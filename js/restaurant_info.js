b4d85c9ae4186bfb940236ca3c969d7eec7df05c
Veniamin Tsigourof <tsigourof_ben@homtail.com>
2018-06-25 14:44:33 +0300

    style: Finished styling layout, site is fully responsive

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -85,7 +85,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   name.innerHTML = restaurant.name;
 
   const address = document.getElementById('restaurant-address');
-  address.innerHTML = restaurant.address;
+  address.innerHTML = 'Address: ' + restaurant.address;
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
